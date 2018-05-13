405402a44797f253b70721d6b7641bc353746faa
Adrien Tournillon <adrien.tournillon@gmail.com>
2018-05-13 18:22:35 +0200

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+""" TODO :
+- Live Score
+- Ameliorer NavBar
+"""
+
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,9 +12,22 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+score_team_1 = 0
+score_team_2 = 10
+
 @app.route('/livematch')
 def live_match():
-    return render_template('livematch.html')
+    return render_template('livematch.html', score_team_1 = score_team_1, score_team_2 = score_team_2)
+
+#Load Players Table
+players_table = pd.read_csv('data/players.csv', sep = ';', index_col = 0)
+@app.route('/players')
+def players():
+    return render_template('players.html', players_table = players_table)
+
+@app.route('/player/<int:id_player>/')
+def player(id_player):
+    return render_template('player.html', players_table = players_table, id_player=id_player)
 
 if __name__ == '__main__':
     app.run(debug=True)
