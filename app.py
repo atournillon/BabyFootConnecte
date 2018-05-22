c8185b90b4f64ee1761751865e07de3c697bd2ab
Thibault Quinquis <37329192+ThibaultQs@users.noreply.github.com>
2018-05-22 17:11:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def player(id_player):
 #Load Choix Equipe
 @app.route('/match_team')
 def match_team():
-    return render_template('livematch.html')
+    return render_template('match_team.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
