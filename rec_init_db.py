3201bee853daf1b479a097573dfa11cf27c0b00c
RiSox22 <32896210+RiSox22@users.noreply.github.com>
2018-06-03 21:00:45 +0200

    Add files via upload

--- rec_init_db.py
+++ rec_init_db.py
@@ -38,19 +38,19 @@ def Trigger():
 def PurgeLiveMatch():
     # Purge de la table contenant le live du match
     requete.execute('''DELETE FROM REC_LIVE_MATCH''')
-    requete.execute('''DELETE FROM REC_LIVE_MATCH_HISTO''')
-    requete.execute('''DELETE FROM REC_REF_PLAYERS''')
+#    requete.execute('''DELETE FROM REC_LIVE_MATCH_HISTO''')
+#    requete.execute('''DELETE FROM REC_REF_PLAYERS''')
 
 # Initialisation des fonctions permettant l'ajouter de données dans une table, manuellement
-def AddData_LIVE(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r):
-    requete.execute('''INSERT INTO REC_LIVE_MATCH (id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r)
-    VALUES (?,?,?,?,?,?,?,?,?)''',(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r))
-def AddData_HISTO(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r):
-    requete.execute('''INSERT INTO REC_LIVE_MATCH_HISTO (id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r)
-    VALUES (?,?,?,?,?,?,?,?,?)''',(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r))
-def AddData_PLAYER(id_player,firstname,lastname,nickname):
-    requete.execute('''INSERT INTO REC_REF_PLAYERS (id_player,firstname,lastname,nickname)
-    VALUES (?,?,?,?)''',(id_player,firstname,lastname,nickname))
+#def AddData_LIVE(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r):
+#    requete.execute('''INSERT INTO REC_LIVE_MATCH (id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r)
+#    VALUES (?,?,?,?,?,?,?,?,?)''',(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r))
+#def AddData_HISTO(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r):
+#    requete.execute('''INSERT INTO REC_LIVE_MATCH_HISTO (id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r)
+#    VALUES (?,?,?,?,?,?,?,?,?)''',(id_match,b1,b2,r1,r2,time_start,time_goal,score_b,score_r))
+#def AddData_PLAYER(id_player,firstname,lastname,nickname):
+#    requete.execute('''INSERT INTO REC_REF_PLAYERS (id_player,firstname,lastname,nickname)
+#    VALUES (?,?,?,?)''',(id_player,firstname,lastname,nickname))
 
 # Appel de la fonction de création des tables
 CreateTable()
@@ -78,64 +78,64 @@ connexion.commit()
 
 # Appel des fonctions d'ajout de données
 # Génération du datetime en INTEGER qui sera le id_match
-time_start=int(time.mktime(datetime.now().timetuple()))
-time_end=time_start+666
-AddData_HISTO(time_start,1,2,3,4,time_start,time_end,4,10)
-
-time_start=time_end+60
-time_end=time_start+666
-AddData_HISTO(time_start,1,7,3,4,time_start,time_end,10,8)
-
-time_start=time_end+60
-time_end=time_start+666
-AddData_HISTO(time_start,5,7,3,6,time_start,time_end,10,9)
-
-time_start=time_end+60
-time_end=time_start+666
-AddData_HISTO(time_start,5,7,3,6,time_start,time_end,10,3)
-AddData_LIVE(time_start,5,7,3,6,time_start,time_end,10,3)
-
-AddData_PLAYER(1,'Eric','BESCOND','RiCo')
-AddData_PLAYER(2,'Thibault','QUINQUIS','TQS')
-AddData_PLAYER(3,'Fabrice','SIMON','Simon')
-AddData_PLAYER(4,'Nicolas','BETIN','BN')
-AddData_PLAYER(5,'Adrien','TOURNILLON','Tournicoti')
-AddData_PLAYER(6,'Mathieu','BROCHARD','La Broche')
-AddData_PLAYER(7,'Mireille','MATHIEU','MiMi')
+#time_start=int(time.mktime(datetime.now().timetuple()))
+#time_end=time_start+666
+#AddData_HISTO(time_start,1,2,3,4,time_start,time_end,4,10)
+#
+#time_start=time_end+60
+#time_end=time_start+666
+#AddData_HISTO(time_start,1,7,3,4,time_start,time_end,10,8)
+#
+#time_start=time_end+60
+#time_end=time_start+666
+#AddData_HISTO(time_start,5,7,3,6,time_start,time_end,10,9)
+#
+#time_start=time_end+60
+#time_end=time_start+666
+#AddData_HISTO(time_start,5,7,3,6,time_start,time_end,10,3)
+#AddData_LIVE(time_start,5,7,3,6,time_start,time_end,10,3)
+#
+#AddData_PLAYER(1,'Eric','BESCOND','RiCo')
+#AddData_PLAYER(2,'Thibault','QUINQUIS','TQS')
+#AddData_PLAYER(3,'Fabrice','SIMON','Simon')
+#AddData_PLAYER(4,'Nicolas','BETIN','BN')
+#AddData_PLAYER(5,'Adrien','TOURNILLON','Tournicoti')
+#AddData_PLAYER(6,'Mathieu','BROCHARD','La Broche')
+#AddData_PLAYER(7,'Mireille','MATHIEU','MiMi')
 
 
 
 # Soumission des fonctions précédemment appelées
-connexion.commit()
+#connexion.commit()
 
 # Affichage du contenu d'une table
-print('\n REC_LIVE_MATCH')
-requete.execute('SELECT * FROM REC_LIVE_MATCH')
-for i in requete:
-    print ('\n')
-    for j in i:
-        print (j)
-        
-print('\n REC_LIVE_MATCH_HISTO')
-requete.execute('SELECT * FROM REC_LIVE_MATCH_HISTO')
-for i in requete:
-    print ('\n')
-    for j in i:
-        print (j)
-
-print('\n REC_REF_PLAYERS')
-requete.execute('SELECT * FROM REC_REF_PLAYERS')
-for i in requete:
-    print ('\n')
-    for j in i:
-        print (j)
-        
-print('\n REC_REF_COMMENTS')
-requete.execute('SELECT * FROM REC_REF_COMMENTS')
-for i in requete:
-    print ('\n')
-    for j in i:
-        print (j)
+#print('\n REC_LIVE_MATCH')
+#requete.execute('SELECT * FROM REC_LIVE_MATCH')
+#for i in requete:
+#    print ('\n')
+#    for j in i:
+#        print (j)
+#        
+#print('\n REC_LIVE_MATCH_HISTO')
+#requete.execute('SELECT * FROM REC_LIVE_MATCH_HISTO')
+#for i in requete:
+#    print ('\n')
+#    for j in i:
+#        print (j)
+#
+#print('\n REC_REF_PLAYERS')
+#requete.execute('SELECT * FROM REC_REF_PLAYERS')
+#for i in requete:
+#    print ('\n')
+#    for j in i:
+#        print (j)
+#        
+#print('\n REC_REF_COMMENTS')
+#requete.execute('SELECT * FROM REC_REF_COMMENTS')
+#for i in requete:
+#    print ('\n')
+#    for j in i:
+#        print (j)
 
 # Déconnexion de la base
 requete.close()
