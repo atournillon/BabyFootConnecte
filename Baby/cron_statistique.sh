cec5912e70edd8edac16fa49070191e652fb07d8
nbetin <39273232+nbetin@users.noreply.github.com>
2018-06-12 15:28:06 +0200

    Revert "Add files via upload"
    This reverts commit 08274403d864386318f3baccaea39e561adba263.

--- Baby/cron_statistique.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if ps -ef | grep -v grep | grep api_statistique.py ; then
-        exit 0
-else
-        source /opt/anaconda/bin/activate py27
-        cd /home/pi/Documents/BabyFootConnecte
-        nohup python statistics/api_statistique.py &
-        exit 0
-fi
--- Baby/help_statistique.py
+++
@@ -1,4 +0,0 @@
-
-import requests
-t=requests.get('http://localhost:3333/calcul_statistique')
-print(t.status_code)
