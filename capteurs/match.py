78cc4e4256d068c9c62455a440d7ce9fa45d1b74
RiSox22 <32896210+RiSox22@users.noreply.github.com>
2018-06-24 22:25:03 +0200

    Update match.py

--- capteurs/match.py
+++ capteurs/match.py
@@ -13,21 +13,24 @@ import json
 import time
 import datetime
 import sys
+from threading import Thread
+from slacker import Slacker
 sys.path.append("data")
 import fonction_database
-
 
 sys.path.append("capteurs/lib")
 import interaction_database
 
 #Import de la librairie pour les capteurs
-#try:
-import RPi.GPIO as GPIO
-#except:
-#    import fake_rpi
-#    sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi (GPIO)
-#    sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
-#    import RPi
+try:
+    import RPi.GPIO as GPIO
+    rpi=1
+except:
+    import fake_rpi
+    sys.modules['RPi'] = fake_rpi.RPi                                   # Fake RPi (GPIO)
+    sys.modules['smbus'] = fake_rpi.smbus                               # Fake smbus (I2C)
+    import RPi
+    rpi=0
 
 
 #Import de la librairie PyGame
@@ -51,15 +54,22 @@ with open('config.json') as conf_file:
     DB = json.load(conf_file)
 
 #Initialisation des capteurs
-#GPIO.setwarnings(False)     #Désactive le Warning
-GPIO.setmode(GPIO.BOARD)    #Mode BCM si on utilise un BreadBoard
-GPIO.setup(DB['capteurs']['id_capteur_bleu'], GPIO.IN)     #Ce Capteur est un Laser sur le PIN 18 - Il est pour les Bleus
-GPIO.setup(DB['capteurs']['id_capteur_rouge'], GPIO.IN)     #Ce Capteur est un Laser sur le PIN 5 - Il est pour les Rouges
+if rpi==0:
+    lg.info("FAKE RPi")
+    #RPi.GPIO.setwarnings(False)                                        #Désactive le Warning
+    RPi.GPIO.setmode(RPi.GPIO.BOARD)                                    #Mode BCM si on utilise un BreadBoard
+    RPi.GPIO.setup(DB['capteurs']['id_capteur_bleu'], RPi.GPIO.IN)      #Ce Capteur est un Laser sur le PIN 18 - Il est pour les Bleus
+    RPi.GPIO.setup(DB['capteurs']['id_capteur_rouge'], RPi.GPIO.IN)     #Ce Capteur est un Laser sur le PIN 5 - Il est pour les Rouges
+else:
+    #GPIO.setwarnings(False)                                            #Désactive le Warning
+    GPIO.setmode(RPi.GPIO.BOARD)                                        #Mode BCM si on utilise un BreadBoard
+    GPIO.setup(DB['capteurs']['id_capteur_bleu'], GPIO.IN)              #Ce Capteur est un Laser sur le PIN 18 - Il est pour les Bleus
+    GPIO.setup(DB['capteurs']['id_capteur_rouge'], GPIO.IN)             #Ce Capteur est un Laser sur le PIN 5 - Il est pour les Rouges
 
 #Initilisation de la manette
 try:
-    pygame.init()                                #Init de Pygame
-    mon_joystick = pygame.joystick.Joystick(0)   #Touche 0 correspond au bouton Start
+    pygame.init()                                                       #Init de Pygame
+    mon_joystick = pygame.joystick.Joystick(0)                          #Touche 0 correspond au bouton Start
     mon_joystick.init()
 except:
     pass
@@ -69,6 +79,66 @@ m = 0
 i = 0
 j = 0
 Last_Goal = 0
+
+class but(Thread):
+    def __init__(self, team, i, j, Last_Goal):
+        Thread.__init__(self)
+        self.team = team
+        self.i = i
+        self.j = j
+        self.Last_Goal = Last_Goal
+
+    def run(self):
+        time_goal = datetime.datetime.now()                                                 #Récupérer le time du but
+        time_goal_str = str('{0:%d/%m/%Y %H:%M:%S}'.format(time_goal))                      #Conversion en format String pour stockage au bon format
+        os.system("mpg321 -q data/audio/sifflet1.mp3")
+        lg.info("Coup de sifflet")
+        if self.team=="bleu":
+            lg.info("Buuuut des Bleus ! {}".format(time_goal_str))
+        elif self.team=="rouge":
+            lg.info("Buuuut des Rouges ! {}".format(time_goal_str))
+        lg.info("{}".format(str(self.i)))
+        lg.info("{}".format(str(self.j)))
+        interaction_database.live(time_goal_str, self.i, self.j, self.Last_Goal)            #Ecriture dans la table live
+        time.sleep(6)                                                                       #On rajoute du temps (5sec) pour éviter les problèmes de détection
+
+class commentaire(Thread):
+    def __init__(self, team, i, j):
+        Thread.__init__(self)
+        self.team = team
+        self.i = i
+        self.j = j
+
+    def run(self):
+        if self.team=="bleu":
+            if self.i == self.j:
+                os.system("mpg321 -q data/audio/egalite.mp3")
+                lg.info("Il y a egalite au score")
+            else:
+                filexist = 0
+                while filexist < 1:
+                    commentaire = requete.execute('''select id_comment from PROD_REF_COMMENTS where (team = 'bleu' or team = 'lesdeux') and scenario = 'but' order by random() limit 1 ''').fetchall()
+                    commentaire_TTS = str(commentaire[0][0])
+                    if os.path.exists("data/audio/com" + commentaire_TTS + ".mp3") == True:
+                        filexist=1
+                        os.system("mpg321 -q data/audio/com" + commentaire_TTS + ".mp3")
+                        lg.info("commentaire data/audio/com" + commentaire_TTS + ".mp3")
+        elif self.team=="rouge":
+            if self.j == self.i:
+                os.system("mpg321 -q data/audio/egalite.mp3")
+                lg.info("Il y a egalite au score")
+            elif self.j-self.i == 5:
+                os.system("mpg321 -q data/audio/allez_les_bleus.mp3")
+                lg.info("Encouragement des bleus qui galerent")
+            else:
+                filexist = 0
+                while filexist < 1:
+                    commentaire = requete.execute('''select id_comment from PROD_REF_COMMENTS where (team = 'rouge' or team = 'lesdeux') and scenario = 'but' order by random() limit 1 ''').fetchall()
+                    commentaire_TTS = str(commentaire[0][0])
+                    if os.path.exists("data/audio/com" + commentaire_TTS + ".mp3") == True:
+                        filexist=1
+                        os.system("mpg321 -q data/audio/com" + commentaire_TTS + ".mp3")
+                        lg.info("Commentaire data/audio/com" + commentaire_TTS + ".mp3")
 
 # 2 - GESTION DE L'ATTENTE D'UN DEBUT DE MATCH_____________________
 # Programme qui tourne à l'infini
@@ -80,15 +150,21 @@ while True:
         requete,connexion = fonction_database.fonction_connexion_sqllite()
         requete.execute("SELECT count(*) FROM PROD_LIVE_MATCH")
         test_score = requete.fetchone()
-        fonction_database.fonction_connexion_sqllite_fermeture(requete,connexion)
+        #fonction_database.fonction_connexion_sqllite_fermeture(requete,connexion)
         nb_rows = test_score[0]
         b, r = interaction_database.read_live()
         i = b
         j = r
 
+        slackClient,channel = fonction_database.fonction_connexion_slack()
+
         if nb_rows > 0 and i == 0 and j == 0:
             # Si elle contient une ligne, c'est qu'un match doit démarrer
             lg.info("On peut démarrer un match")
+
+            # Lancement des festivites
+            os.system("mpg321 -q data/audio/ea_sport.mp3")
+            lg.info("Son EA Sport")
 
             #################################
             # 3 - DEROULE DU MATCH
@@ -112,41 +188,79 @@ while True:
 
 
             #Boucle pour créer le match jusqu'au moment où une équipe arrive à 10
-            while  i < 10 and j < 10:                                                       #Boucle de 10 buts
-                #Buts pour les bleus
-                if GPIO.input(DB['capteurs']['id_capteur_bleu']) == 0:
-                    
-                    time_goal = datetime.datetime.now()                                     #Récupérer le time du but
-                    time_goal_str = str('{0:%d/%m/%Y %H:%M:%S}'.format(time_goal))          #Conversion en format String pour stockage au bon format
-                    b, r = interaction_database.read_live()
-                    i = b + 1                                                                  #Incrément du but marqué
-                    j = r
-                    Last_Goal = 1															#Ce but a été marqué par les bleus - utiliser pour l'annulation
-                    lg.info("Buuuut des Bleus ! {}".format(time_goal_str))
-                    lg.info("{}".format(str(i)))
-                    lg.info("{}".format(str(j)))
-                    interaction_database.live(time_goal_str, i, j, Last_Goal)               #Ecriture dans la table live
-                    time.sleep(5)                                                           #On rajoute du temps (5sec) pour éviter les problèmes de détection
+            while  i < 10 and j < 10:                                                           #Boucle de 10 buts
+                if rpi==0:
+                    #Buts pour les bleus
+                    if RPi.GPIO.input(DB['capteurs']['id_capteur_bleu']) == 0:
+                        b, r = interaction_database.read_live()
+                        i = b + 1                                                               #Incrément du but marqué
+                        j = r
+                        Last_Goal = 1                                                           #Ce but a été marqué par les bleus - utiliser pour l'annulation
+                        thread_1 = but("bleu",i,j,Last_Goal)
+                        thread_2 = commentaire("bleu",i,j)
+                        lg.info("Démarrage Thread 1 BUT")
+                        lg.info("Démarrage Thread 2 COMMENTAIRE")
+                        thread_1.start()
+                        thread_2.start()
+                        thread_1.join()
+                        thread_2.join()
+                        lg.info("Fin Thread 1 BUT")
+                        lg.info("Fin Thread 2 COMMENTAIRE")
 
-                #Buts pour les rouges
-                if GPIO.input(DB['capteurs']['id_capteur_rouge']) == 0:                     #Détection des mouvements sur le PIN 19
-                                       
-                    time_goal = datetime.datetime.now()                                     #Récupérer le time du but
-                    time_goal_str = str('{0:%d/%m/%Y %H:%M:%S}'.format(time_goal))          #Conversion en format String pour stockage au bon format
-                    b, r = interaction_database.read_live()
-                    j = r + 1                                                                #Incrément du but marqué
-                    i = b
-                    Last_Goal = 2															#Ce but a été marqué par les bleus - utiliser pour l'annulation
-                    lg.info("Buuuut des Rouges ! {}".format(time_goal_str))
-                    lg.info("{}".format(str(i)))
-                    lg.info("{}".format(str(j)))
-                    interaction_database.live(time_goal_str, i, j, Last_Goal)                #Ecriture dans la table live
-                    time.sleep(5)                                                           #On rajoute du temps (5sec) pour éviter les problèmes de détection
+                    #Buts pour les rouges
+                    if RPi.GPIO.input(DB['capteurs']['id_capteur_rouge']) == 0:                 
+                        b, r = interaction_database.read_live()
+                        j = r + 1                                                               #Incrément du but marqué
+                        i = b
+                        Last_Goal = 2                                                           #Ce but a été marqué par les bleus - utiliser pour l'annulation
+                        thread_1 = but("rouge",i,j,Last_Goal)
+                        thread_2 = commentaire("rouge",i,j)
+                        lg.info("Démarrage Thread 1 BUT")
+                        lg.info("Démarrage Thread 2 COMMENTAIRE")
+                        thread_1.start()
+                        thread_2.start()
+                        thread_1.join()
+                        thread_2.join()
+                        lg.info("Fin Thread 1 BUT")
+                        lg.info("Fin Thread 2 COMMENTAIRE")
+                else:
+                    #Buts pour les bleus
+                    if GPIO.input(DB['capteurs']['id_capteur_bleu']) == 0:
+                        b, r = interaction_database.read_live()
+                        i = b + 1                                                               #Incrément du but marqué
+                        j = r
+                        Last_Goal = 1                                                           #Ce but a été marqué par les bleus - utiliser pour l'annulation
+                        thread_1 = but("bleu",i,j,Last_Goal)
+                        thread_2 = commentaire("bleu",i,j)
+                        lg.info("Démarrage Thread 1 BUT")
+                        lg.info("Démarrage Thread 2 COMMENTAIRE")
+                        thread_1.start()
+                        thread_2.start()
+                        thread_1.join()
+                        thread_2.join()
+                        lg.info("Fin Thread 1 BUT")
+                        lg.info("Fin Thread 2 COMMENTAIRE")
 
+                    #Buts pour les rouges
+                    if GPIO.input(DB['capteurs']['id_capteur_rouge']) == 0:                     
+                        b, r = interaction_database.read_live()
+                        j = r + 1                                                               #Incrément du but marqué
+                        i = b
+                        Last_Goal = 2                                                           #Ce but a été marqué par les bleus - utiliser pour l'annulation
+                        thread_1 = but("rouge",i,j,Last_Goal)
+                        thread_2 = commentaire("rouge",i,j)
+                        lg.info("Démarrage Thread 1 BUT")
+                        lg.info("Démarrage Thread 2 COMMENTAIRE")
+                        thread_1.start()
+                        thread_2.start()
+                        thread_1.join()
+                        thread_2.join()
+                        lg.info("Fin Thread 1 BUT")
+                        lg.info("Fin Thread 2 COMMENTAIRE")  
                 try:
                     #Annulation du dernier but
                     for event in pygame.event.get():                                            #A tout moment on peut annuler un but
-                        if event.type == pygame.JOYBUTTONDOWN:                                         #Quand un bouton est appuyé
+                        if event.type == pygame.JOYBUTTONDOWN:                                  #Quand un bouton est appuyé
                             if event.button == 0:                                               #Le bouton 0 correspond au bouton X
                                 if Last_Goal == 1:                                              #Si le dernier but vient des bleus
                                     i = i - 1                                                   #On retire le but
@@ -171,11 +285,28 @@ while True:
                     #lg.info('pas de joystick')
 # 4 - FIN DU MATCH
 
+            os.system("mpg321 -q data/audio/applaudissements1.mp3")
+            lg.info("Applaudissements car fin du match")
+
             # Affichage du résultat
-            if i == 10:                                                                                 #Si les bleus arrivent à 10 buts
+            if i == 10:                                                                          #Si les bleus arrivent à 10 buts
+                fonction_database.fonction_connexion_sqllite_fermeture(requete,connexion)
                 lg.info("C'est donc terminé pour ce match. Victoire des Bleus")
-            elif j == 10:                                                                               #Si les rouge arrivent à 10 buts
+                if rpi==0:
+                    messageToChannel = "[test local] Hello, Victoire des Bleus " + str(i) + " - " + str(j)
+                else:
+                    messageToChannel = "Hello, Victoire des Bleus " + str(i) + " - " + str(j)
+                slackClient.chat.post_message(channel,messageToChannel)
+                lg.info("Push du match sur slack")
+            elif j == 10:                                                                        #Si les rouge arrivent à 10 buts
+                fonction_database.fonction_connexion_sqllite_fermeture(requete,connexion)
                 lg.info("C'est donc terminé pour ce match. Victoire des Rouges")
+                if rpi==0:
+                    messageToChannel = "[test local] Hello, Victoire des Rouges " + str(j) + " - " + str(i)
+                else:
+                    messageToChannel = "Hello, Victoire des Rouges " + str(j) + " - " + str(i)
+                slackClient.chat.post_message(channel,messageToChannel)
+                lg.info("Push du match sur Slack")
 
     except Exception as e:
         lg.error("PROBLEME SUR LA TABLE LIVE")
