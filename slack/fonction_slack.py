5583a63d397844b2e75f326b799f8f602defbb50
RiSox22 <32896210+RiSox22@users.noreply.github.com>
2018-06-25 21:22:40 +0200

    Update fonction_slack.py

--- slack/fonction_slack.py
+++ slack/fonction_slack.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python
+#coding: utf8
 
+import json
+import datetime
+from slacker import Slacker
+
+with open('config.json') as conf_file:
+    global DB
+    DB = json.load(conf_file)
+
+def fonction_connexion_slack(choose):
+    #Fonction pour se connecter à slack
+    token_slack = DB['slack']['token']
+    slackClient = Slacker(token_slack)
+    channel = DB['slack'][choose]
+    return slackClient,channel
