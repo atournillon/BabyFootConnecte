18daeed5bd22fa09da735888ee60dc9e8fee3c40
FabSN <fabrice.simon1@gmail.com>
2018-06-25 15:21:53 +0200

    f

---
+++ temperature_rasp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding: utf8
+
+from subprocess import PIPE, Popen
+import json
+from slacker import Slacker
+
+with open('config.json') as conf_file:
+    global DB
+    DB = json.load(conf_file)
+
+token_slack = DB['slack']['token']
+slackClient = Slacker(token_slack)
+channel = DB['slack']['channel']
+
+def get_cpu_temperature():
+    """get cpu temperature using vcgencmd"""
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+slackClient.chat.post_message(channel,str(get_cpu_temperature()))
