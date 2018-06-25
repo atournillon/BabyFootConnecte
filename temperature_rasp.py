95be0c38b86af96749ed58d8aee21ab4610319f1
RiSox22 <32896210+RiSox22@users.noreply.github.com>
2018-06-25 16:57:35 +0200

    Update temperature_rasp.py

--- temperature_rasp.py
+++ temperature_rasp.py
@@ -4,14 +4,14 @@
 from subprocess import PIPE, Popen
 import json
 from slacker import Slacker
+sys.path.append("data")
+import fonction_database
 
 with open('config.json') as conf_file:
     global DB
     DB = json.load(conf_file)
 
-token_slack = DB['slack']['token']
-slackClient = Slacker(token_slack)
-channel = DB['slack']['channel']
+slackClient,channel_temp = fonction_database.fonction_temperature_slack()
 
 def get_cpu_temperature():
     """get cpu temperature using vcgencmd"""
@@ -19,4 +19,4 @@ def get_cpu_temperature():
     output, _error = process.communicate()
     return float(output[output.index('=') + 1:output.rindex("'")])
 
-slackClient.chat.post_message(channel,"La température est de : "+str(get_cpu_temperature())+" °C")
+slackClient.chat.post_message(channel_temp,"La température est de : "+str(get_cpu_temperature())+" °C")
